3b870099df6fecd0c366e613c99514c813eedf4a
mai <thisismainguyen@gmail.com>
2025-04-09 14:17:23 -0400

    add validation to query params

--- api/backend/organizations/controllers.py
+++ api/backend/organizations/controllers.py
@@ -1,8 +1,7 @@
 from http import HTTPStatus
-from typing import Tuple
-from api.backend.utilities.errors import handle_error
-from api.backend.utilities.uuid import validate_uuid
-from api.backend.validators.programs import ProgramSchema
+from backend.utilities.errors import handle_error
+from backend.utilities.uuid import validate_uuid
+from programs import ProgramSchema
 from flask import Blueprint, request, jsonify, Response
 
 #------------------------------------------------------------
@@ -13,16 +12,13 @@ organizations = Blueprint('organizations', __name__)
 #------------------------------------------------------------
 # Create a program for an organization
 @organizations.route('/<string:id>/programs', methods=['POST'])
-def create_program(id: str) -> Tuple[Response, int]:
+def create_program(id: str) -> tuple[Response, int]:
     try:
-        # validate if it is a valid UUID
         validate_uuid(id)
 
-        # validate request body
         program_schema = ProgramSchema()
         data = program_schema.load(request.json)
 
-        # create program using transaction
         result = insert_program(data)
         return jsonify(result), HTTPStatus.CREATED
     
--- api/backend/programs/controllers.py
+++ api/backend/programs/controllers.py
@@ -3,7 +3,8 @@ from flask import Blueprint, request, jsonify
 from backend.utilities.errors import handle_error
 from backend.utilities.uuid import validate_uuid
 from http import HTTPStatus
-from typing import Tuple, Response
+from typing import Response
+from api.backend.validators.search import validate_search_params
 
 #------------------------------------------------------------
 # Create a new Blueprint object, which is a collection of 
@@ -13,7 +14,7 @@ programs = Blueprint('programs', __name__)
 #------------------------------------------------------------
 # Get a specific program
 @programs.route('/<string:program_id>', methods=['GET'])
-def get_program(program_id: str) -> Tuple[Response, int]:
+def get_program(program_id: str) -> tuple[Response, int]:
     try:
         validate_uuid(program_id)
         program = retrieve_program(program_id)
@@ -25,15 +26,11 @@ def get_program(program_id: str) -> Tuple[Response, int]:
 #------------------------------------------------------------
 # Get a specific program
 @programs.route('/search', methods=['GET'])
-def search() -> Tuple[Response, int]:
+def search() -> tuple[Response, int]:
     try:
-        page, limit = validate_pagination()
-
-        # TODO: handle validation of query params
-
-        program = search_program(page, limit, categories, location, search_term, is_qualified, sort_by)
-
-        return jsonify(program), HTTPStatus.OK
+        validated_params = validate_search_params()
+        programs = search_program(validated_params)
+        return jsonify(programs), HTTPStatus.OK
 
     except Exception as e:
         return handle_error(e)
@@ -41,7 +38,7 @@ def search() -> Tuple[Response, int]:
 #------------------------------------------------------------
 # Get a specific program's feedback forms
 @programs.route('/<string:program_id>/feedbacks', methods=['GET'])
-def get_feedback(program_id: str) -> Tuple[Response, int]:
+def get_feedback(program_id: str) -> tuple[Response, int]:
     try:
         validate_uuid(program_id)
         page, limit = validate_pagination()
@@ -54,7 +51,7 @@ def get_feedback(program_id: str) -> Tuple[Response, int]:
 #------------------------------------------------------------
 # Get all user profiles who apply to specific program
 @programs.route('/<string:program_id>/profiles', methods=['GET'])
-def get_profiles(program_id: str) -> Tuple[Response, int]:
+def get_profiles(program_id: str) -> tuple[Response, int]:
     try:
         validate_uuid(program_id)
         page, limit = validate_pagination()
@@ -67,7 +64,7 @@ def get_profiles(program_id: str) -> Tuple[Response, int]:
 #------------------------------------------------------------
 # Get all applications to a specific program
 @programs.route('/<string:program_id>/applications', methods=['GET'])
-def get_applications(program_id: str) -> Tuple[Response, int]:
+def get_applications(program_id: str) -> tuple[Response, int]:
     try:
         validate_uuid(program_id)
         page, limit = validate_pagination()
@@ -80,7 +77,7 @@ def get_applications(program_id: str) -> Tuple[Response, int]:
 #------------------------------------------------------------
 # Get the stats of how applications change over time
 @programs.route('/<string:program_id>/applications/stats', methods=['GET'])
-def get_applications_stats(program_id: str) -> Tuple[Response, int]:
+def get_applications_stats(program_id: str) -> tuple[Response, int]:
     try:
         validate_uuid(program_id)
         page, limit = validate_pagination()
@@ -93,9 +90,9 @@ def get_applications_stats(program_id: str) -> Tuple[Response, int]:
 #------------------------------------------------------------
 # Get the retention of programs
 @programs.route('/<string:program_id>/retention', methods=['GET'])
-def get_retention(program_id: str) -> Tuple[Response, int]:
+def get_retention(program_id: str) -> tuple[Response, int]:
     try:
-        validate_uuid(program_id)        # validate pagination params
+        validate_uuid(program_id) 
         page, limit = validate_pagination()
         program = get_program_retention(program_id, page, limit)
         return jsonify(program), HTTPStatus.OK
@@ -106,11 +103,12 @@ def get_retention(program_id: str) -> Tuple[Response, int]:
 #------------------------------------------------------------
 # Update a specific program
 @programs.route('/<string:program_id>', methods=['PUT'])
-def update_program(program_id):
+def update_program(program_id) -> tuple[Response, int]:
     try:
-        # TODO: add validation of request body here
-        data = request.json
-        updated_program = update_program_info(data)
+        validate_uuid(program_id)
+        program_schema = UpdateProgramSchema()
+        data = program_schema.load(request.json)
+        updated_program = update_program_info(program_id, data)
         return jsonify(updated_program), HTTPStatus.OK
     
     except Exception as e:
@@ -119,7 +117,7 @@ def update_program(program_id):
 #------------------------------------------------------------
 # Delete a specific program
 @programs.route('/<string:program_id>', methods=['DELETE'])
-def delete_program(program_id: str):
+def delete_program(program_id: str) -> tuple[Response, int]:
     try:
         validate_uuid(program_id)
         remove_program(program_id)
