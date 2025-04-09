a54bec1d51f0c2a0bd2802dcfa343114bb1ce62b
tGiech22 <tonygiech@gmail.com>
2025-04-09 17:27:34 -0400

    successfully implemented update user endpoint

--- api/backend/users/validators.py
+++ api/backend/users/validators.py
@@ -10,3 +10,9 @@ class UserSchema(Schema):
         validate=validate.OneOf(['admin', 'user', 'data_analyst', 'organization_admin'])
     )
     registered_at = fields.DateTime(dump_only=True)  # automatically set by the database
+
+class UserUpdateSchema(Schema):
+    """Schema for validating user update data."""
+    first_name = fields.String(validate=validate.Length(min=1, max=50))
+    last_name = fields.String(validate=validate.Length(min=1, max=50))
+    type = fields.String(validate=validate.OneOf(['admin', 'user', 'data_analyst', 'organization_admin']))
