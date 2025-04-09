283ed150ca295bcff540f9a58a7c51dfa3a01796
mai <thisismainguyen@gmail.com>
2025-04-09 13:32:50 -0400

    finish setting up majority of controllers

--- api/backend/organizations/controllers.py
+++ api/backend/organizations/controllers.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, request, jsonify
-from flask import make_response
-from flask import current_app
-from backend.database import db
+from http import HTTPStatus
+from typing import Tuple
+from api.backend.utilities.errors import handle_error
+from api.backend.utilities.uuid import validate_uuid
+from api.backend.validators.programs import ProgramSchema
+from flask import Blueprint, request, jsonify, Response
 
 #------------------------------------------------------------
 # Create a new Blueprint object, which is a collection of 
@@ -11,7 +13,7 @@ organizations = Blueprint('organizations', __name__)
 #------------------------------------------------------------
 # Create a program for an organization
 @organizations.route('/<string:id>/programs', methods=['POST'])
-def create_program(id: str):
+def create_program(id: str) -> Tuple[Response, int]:
     try:
         # validate if it is a valid UUID
         validate_uuid(id)
@@ -21,7 +23,7 @@ def create_program(id: str):
         data = program_schema.load(request.json)
 
         # create program using transaction
-        result = create_program(id)
+        result = insert_program(data)
         return jsonify(result), HTTPStatus.CREATED
     
     except Exception as e:
