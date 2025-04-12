4d542d156a16905beeb7bab4ba4ddcab686c2720
Xinning Liu <147454675+6r3ad@users.noreply.github.com>
2025-04-12 13:01:52 -0400

    endpoints for organization and organization contact

--- api/backend/organizations/controllers.py
+++ api/backend/organizations/controllers.py
@@ -1,5 +1,12 @@
 from http import HTTPStatus
-from backend.organizations.transactions import insert_program
+from backend.validators.organizations import OrganizationSchema, OrganizationContactSchema
+from backend.organizations.transactions import (
+    create_organization, 
+    insert_program, 
+    get_organization_by_id, 
+    delete_organization_by_id, 
+    update_organization_by_id
+)
 from backend.utilities.errors import handle_error
 from backend.utilities.uuid import validate_uuid
 from flask import Blueprint, request, jsonify, Response
@@ -13,7 +20,6 @@ organizations = Blueprint('organizations', __name__)
 def create_program(id: str) -> tuple[Response, int]:
     try:
         validate_uuid(id)
-
         program_schema = ProgramCreateSchema()
         data = program_schema.load(request.json)
 
@@ -22,3 +28,98 @@ def create_program(id: str) -> tuple[Response, int]:
     
     except Exception as e:
         return handle_error(e)
+    
+#------------------------------------------------------------
+# Create an organization
+@organizations.route('', methods=['POST'])
+def create_organizations()  -> tuple[Response, int]:
+    try:
+        organizations_schema = OrganizationSchema()
+        data = organizations_schema.load(request.json)
+        result = create_organization(data)
+        return jsonify(result), HTTPStatus.CREATED
+
+    except Exception as e:
+        return handle_error(e)
+    
+#------------------------------------------------------------
+# Get an organization
+@organizations.route('/<string:organization_id>', methods=['GET'])       
+def get_organization(organization_id: str) -> tuple[Response, int]:
+    try:
+        validate_uuid(organization_id)
+        organization = get_organization_by_id(organization_id)
+        return jsonify(organization), HTTPStatus.CREATED
+
+    except Exception as e:
+        return handle_error(e)
+    
+#------------------------------------------------------------
+# Update an organization
+@organizations.route('/<string:organization_id>', methods=['PUT'])       
+def update_organization(organization_id: str) -> tuple[Response, int]:
+    try:
+        validate_uuid(organization_id)
+        organization = update_organization_by_id(organization_id)
+        return jsonify(organization), HTTPStatus.CREATED
+
+    except Exception as e:
+        return handle_error(e)
+
+#------------------------------------------------------------
+# Delete an organization
+@organizations.route('/<string:organization_id>', methods=['DELETE'])       
+def delete_organization(organization_id: str) -> tuple[Response, int]:
+    try:
+        validate_uuid(organization_id)
+        delete_organization_by_id(organization_id)
+        return jsonify({"message": "Organization deleted successfully"}), HTTPStatus.CREATED
+
+    except Exception as e:
+        return handle_error(e)
+    
+#------------------------------------------------------------
+# Create organization contact
+@organizations.route('/<string:organization_id>/<string:contact_id>', methods=['POST'])
+def create_org_contacts(organization_id: str, contact_id: str) -> tuple[Response, int]:
+    try:
+        validate_uuid(organization_id)
+        validate_uuid(contact_id)
+        organizations_contact_schema = OrganizationContactSchema()
+        data = organizations_contact_schema.load(request.json)
+        new_organization_contact = create_organization_contact(data)
+        return jsonify(new_organization_contact), HTTPStatus.CREATED
+
+    except Exception as e:
+        return handle_error(e)
+    
+#------------------------------------------------------------
+# Get organization contact
+@organizations.route('/<string:organization_id>/<string:contact_id>', methods=['GET'])
+def get_org_contacts(organization_id: str, contact_id: str) -> tuple[Response, int]:
+    try:
+        validate_uuid(organization_id)
+        validate_uuid(contact_id)
+        organizations_contact_schema = OrganizationContactSchema()
+        data = organizations_contact_schema.load(request.json)
+        organization_contact = get_organization_contact(data)
+        return jsonify(organization_contact), HTTPStatus.CREATED
+
+    except Exception as e:
+        return handle_error(e)
+    
+#------------------------------------------------------------
+# Delete organization contact
+@organizations.route('/<string:organization_id>/<string:contact_id>', methods=['DELETE'])
+def delete_org_contacts(organization_id: str, contact_id: str) -> tuple[Response, int]:
+    try:
+        validate_uuid(organization_id)
+        validate_uuid(contact_id)
+        organizations_contact_schema = OrganizationContactSchema()
+        data = organizations_contact_schema.load(request.json)
+        delete_organization_contact(data)
+        return jsonify({"message": "Organization deleted successfully"}), HTTPStatus.CREATED
+
+    except Exception as e:
+        return handle_error(e)
+    
--- api/backend/organizations/transactions.py
+++ api/backend/organizations/transactions.py
@@ -1,7 +1,7 @@
 from typing import Dict, Any, List, Optional
 from backend.programs.transactions import retrieve_program
 from backend.database import db
-from backend.utilities.errors import DatabaseError, ValidationError
+from backend.utilities.errors import ConflictError, DatabaseError, NotFoundError, ValidationError
 from mysql.connector import Error as MySQLError
 
 def insert_program(organization_id: str, data: Dict[str, Any]) -> None:
@@ -21,5 +21,81 @@ def insert_program(organization_id: str, data: Dict[str, Any]) -> None:
         db.get_db().rollback()
         print(f"Unexpected error: {str(e)}")
         raise DatabaseError(f"Unexpected error: {str(e)}")
+    finally:
+        cursor.close()
+
+def create_organization(data: Dict[str, str]) -> Dict[str, Any]:
+    cursor = db.get_db().cursor()
+    try: 
+        cursor.execute('INSERT INTO organizations (name, description, website_url) VALUES (%s, %s, %s)', 
+                       (data['name'], data['description'], data['website_url']))
+        db.get_db().commit()
+        cursor.execute('SELECT * FROM organizations WHERE name = %s', (data['name'],))
+        result = cursor.fetchone()
+        if not result:
+            raise DatabaseError("Failed to create organization")
+        return result
+    except MySQLError as e:
+        if e.args[0] == 1062:
+            raise ConflictError(f"Organization with name {data['name']} already exists")
+        else:
+            raise DatabaseError(str(e))
+    finally:
+        cursor.close()
+
+
+def get_organization_by_id(organization_id: str):
+    cursor = db.get_db().cursor()
+    try:
+        cursor.execute('SELECT * FROM organizations WHERE id = %s', (organization_id,))
+        result = cursor.fetchone()
+        if not result:
+            raise NotFoundError(f"Organization with id {organization_id} does not exist")
+        return result
+    except MySQLError as e:
+        raise DatabaseError(str(e))
+    finally:
+        cursor.close()
+
+def update_organization_by_id(organization_id: str, update_data: Dict[str, Any]) -> Dict[str, Any]:
+    cursor = db.get_db().cursor()
+    try: 
+        update_fields = []
+        values = []
+        for key in ['name, description, website_url, is_verified']:
+            if key in update_data:
+                update_fields.append(f"{key} = %s")
+                values.append(update_data[key])
+        if not update_fields:
+            raise DatabaseError("No valid fields to update")
+        values.append(organization_id)
+        query = f"UPDATE organization SET {', '.join(update_fields)} WHERE id = %s"
+        cursor.execute(query, values)
+        db.get_db().commit()
+
+         # Fetch the updated organization
+        cursor.execute('SELECT * FROM categories WHERE id = %s', (organization_id,))
+        result = cursor.fetchone()
+        if not result:
+            raise NotFoundError(f"Organization with id {organization_id} does not exist")
+        return result
+    except MySQLError as e:
+        if e.args[0] == 1062:
+            raise ConflictError(f"Organization with name {update_data['name']} already exists")
+        else:
+            raise DatabaseError(str(e))
+    finally:
+        cursor.close()
+        
+
+    
+        
+def delete_organization_by_id(organization_id: str):
+    cursor = db.get_db().cursor()
+    try:
+        cursor.execute('DELETE FROM organizations WHERE id = %s', (organization_id))
+        db.get_db().commit()
+    except MySQLError as e:
+        raise DatabaseError(str(e))
     finally:
         cursor.close()
